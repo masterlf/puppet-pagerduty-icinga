a1cde5c63b3a2f646f93a1c64a1b9d06fbd96508
lauretf <noreply@puppet.com>
2019-08-26 16:19:57 +0300

    packages collision troubleshooting

--- manifests/package.pp
+++ manifests/package.pp
@@ -4,6 +4,5 @@
 #   include pagerduty::package
 class pagerduty::package {
   #install packages if not present
-  if ! defined(Package['libwww-perl']) {package {'libwww-perl': ensure => present }}
-  if ! defined(Package['libcrypt-ssleay-perl']) {package {'libcrypt-ssleay-perl': ensure => present }}
+  ensure_packages(['libwww-perl', 'libcrypt-ssleay-perl'], {'ensure' => 'present'})
 }
