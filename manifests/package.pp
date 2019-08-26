647e1757eac905ed750b2cdcbff8e46421672d6a
lauretf <noreply@puppet.com>
2019-08-26 19:47:05 +0300

    debugging

--- manifests/package.pp
+++ manifests/package.pp
@@ -6,7 +6,7 @@ class pagerduty::package (
   String $manage_packages = $::pagerduty::params::manage_packages,
   ){
   #install packages if not present
-  If $manage_packages == 'yes' {
+  if $manage_packages == 'yes' {
     ensure_packages(['libwww-perl', 'libcrypt-ssleay-perl'], {'ensure' => 'present'})
   }
 }
